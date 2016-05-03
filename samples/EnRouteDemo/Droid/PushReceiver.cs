db555b893d8f9ee3b328e54f0c922c9fb97b630e
Nickolas Glickenhouse <nick@glympse.com>
2016-05-03 15:02:33 -0700

    Fixed tabs to spaces

--- samples/EnRouteDemo/Droid/PushReceiver.cs
+++ samples/EnRouteDemo/Droid/PushReceiver.cs
@@ -17,44 +17,44 @@ using Glympse.EnRoute.Android;
 
 namespace EnRouteDemo.Droid
 {
-	[BroadcastReceiver (Name="com.glympse.android.PushReceiver")]
-	[IntentFilter(new[] { "com.glympse.android.hal.service.STARTED", "com.glympse.android.hal.push.REFRESH", "com.glympse.android.hal.push.DATA" })]
-	public class PushReceiver : BroadcastReceiver
-	{
-		private const String INTENT_STARTED = "com.glympse.android.hal.service.STARTED";
-		private const String INTENT_REFRESH = "com.glympse.android.hal.push.REFRESH";
-		private const String INTENT_DATA = "com.glympse.android.hal.push.DATA";
+    [BroadcastReceiver (Name="com.glympse.android.PushReceiver")]
+    [IntentFilter(new[] { "com.glympse.android.hal.service.STARTED", "com.glympse.android.hal.push.REFRESH", "com.glympse.android.hal.push.DATA" })]
+    public class PushReceiver : BroadcastReceiver
+    {
+        private const String INTENT_STARTED = "com.glympse.android.hal.service.STARTED";
+        private const String INTENT_REFRESH = "com.glympse.android.hal.push.REFRESH";
+        private const String INTENT_DATA = "com.glympse.android.hal.push.DATA";
 
-		public override void OnReceive (Context context, Intent intent)
-		{
-			String action = intent.Action;
-			Log.Info("PushReceiver", action);
+        public override void OnReceive (Context context, Intent intent)
+        {
+            String action = intent.Action;
+            Log.Info("PushReceiver", action);
 
-			if ( INTENT_STARTED == action ) 
-			{
-				startManager(context);
-			} 
-			else if ( INTENT_REFRESH == action ) 
-			{
-				startManager(context);
-			} 
-			else if ( INTENT_DATA == action ) 
-			{
-				startManager(context);
-			}
-		}
+            if ( INTENT_STARTED == action ) 
+            {
+                startManager(context);
+            } 
+            else if ( INTENT_REFRESH == action ) 
+            {
+                startManager(context);
+            } 
+            else if ( INTENT_DATA == action ) 
+            {
+                startManager(context);
+            }
+        }
 
-		private void startManager(Context context)
-		{
-			GEnRouteFactory enRouteFactory = new EnRouteFactory (context);
-			EnRouteManagerWrapper.Instance.create(enRouteFactory);
+        private void startManager(Context context)
+        {
+            GEnRouteFactory enRouteFactory = new EnRouteFactory (context);
+            EnRouteManagerWrapper.Instance.create(enRouteFactory);
 
-			GEnRouteManager enRouteManager = EnRouteManagerWrapper.Instance.Manager;
-			if ( enRouteManager.isLoginNeeded() || !enRouteManager.isStarted() ) 
-			{
-				Auth.onAppStart(enRouteManager);
-			}
-		}
-	}
+            GEnRouteManager enRouteManager = EnRouteManagerWrapper.Instance.Manager;
+            if ( enRouteManager.isLoginNeeded() || !enRouteManager.isStarted() ) 
+            {
+                Auth.onAppStart(enRouteManager);
+            }
+        }
+    }
 }
-
+    
--- samples/EnRouteDemo/EnRouteManagerWrapper.cs
+++ samples/EnRouteDemo/EnRouteManagerWrapper.cs
@@ -6,46 +6,46 @@ using Glympse.EnRoute;
 
 namespace EnRouteDemo
 {
-	public class EnRouteManagerWrapper
-	{
-		private static EnRouteManagerWrapper instance;
+    public class EnRouteManagerWrapper
+    {
+        private static EnRouteManagerWrapper instance;
 
-		private GEnRouteFactory _enRouteFactory;
+        private GEnRouteFactory _enRouteFactory;
 
-		private GEnRouteManager _enRouteManager;
+        private GEnRouteManager _enRouteManager;
 
-		private EnRouteManagerWrapper ()
-		{
-		}
+        private EnRouteManagerWrapper()
+        {
+        }
 
-		public static EnRouteManagerWrapper Instance
-		{
-			get 
-			{
-				if (null == instance) 
-				{
-					instance = new EnRouteManagerWrapper();
-				}
-				return instance;
-			}
-		}
+        public static EnRouteManagerWrapper Instance
+        {
+            get 
+            {
+                if ( null == instance ) 
+                {
+                    instance = new EnRouteManagerWrapper();
+                }
+                return instance;
+            }
+        }
 
-		public void create(GEnRouteFactory enRouteFactory)
-		{
-			if (null == _enRouteFactory || null == _enRouteManager) 
-			{
-				_enRouteFactory = enRouteFactory;
-				_enRouteManager = _enRouteFactory.createEnRouteManager();
-			}
-		}
+        public void create(GEnRouteFactory enRouteFactory)
+        {
+            if ( null == _enRouteFactory || null == _enRouteManager ) 
+            {
+                _enRouteFactory = enRouteFactory;
+                _enRouteManager = _enRouteFactory.createEnRouteManager();
+            }
+        }
 
-		public GEnRouteManager Manager
-		{
-			get 
-			{
-				return _enRouteManager;
-			}
-		}
-	}
+        public GEnRouteManager Manager
+        {
+            get 
+            {
+                return _enRouteManager;
+            }
+        }
+    }
 }
 
