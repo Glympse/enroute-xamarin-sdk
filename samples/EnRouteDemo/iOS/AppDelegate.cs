530bcdb339b947fe930c1a88237b1cd4eaeff1f5
Nickolas Glickenhouse <nick@glympse.com>
2016-06-21 16:27:09 -0700

    Enabled Push notifications for sample app. App must be signed with a provisioning profile created for a certificate that supports push. The bundle ID must be set correctly for this provisioning profile as well. Valid credentials must be added to the Auth.cs snippet too.

--- samples/EnRouteDemo/iOS/AppDelegate.cs
+++ samples/EnRouteDemo/iOS/AppDelegate.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Diagnostics;
 
 using Foundation;
 using UIKit;
@@ -20,8 +21,67 @@ namespace EnRouteDemo.iOS
             GEnRouteFactory enRouteFactory = new EnRouteFactory();
 
             LoadApplication (new App (enRouteFactory));
+            
+            // Register for a Push notification token
+            if (UIDevice.CurrentDevice.CheckSystemVersion (8, 0)) 
+            {
+                var pushSettings = UIUserNotificationSettings.GetSettingsForTypes (
+                                   UIUserNotificationType.Alert | UIUserNotificationType.Badge | UIUserNotificationType.Sound,
+                                   new NSSet ());
+            
+                UIApplication.SharedApplication.RegisterUserNotificationSettings (pushSettings);
+                UIApplication.SharedApplication.RegisterForRemoteNotifications ();
+            } 
+            else 
+            {
+                UIRemoteNotificationType notificationTypes = UIRemoteNotificationType.Alert | UIRemoteNotificationType.Badge | UIRemoteNotificationType.Sound;
+                UIApplication.SharedApplication.RegisterForRemoteNotificationTypes (notificationTypes);
+            }
 
             return base.FinishedLaunching (app, options);
+        }
+        
+        public override void RegisteredForRemoteNotifications (UIApplication application, NSData deviceToken)
+        {
+            // Get current device token
+            var DeviceToken = deviceToken.Description;
+            if (!string.IsNullOrWhiteSpace(DeviceToken)) 
+            {
+                DeviceToken = DeviceToken.Trim('<').Trim('>');
+            }
+            
+            Debug.WriteLine(DeviceToken);
+        
+            // Get previous device token
+            var oldDeviceToken = NSUserDefaults.StandardUserDefaults.StringForKey("PushDeviceToken");
+        
+            // Has the token changed?
+            if (string.IsNullOrEmpty(oldDeviceToken) || !oldDeviceToken.Equals(DeviceToken))
+            {
+                Debug.WriteLine("Sharing device token with EnRoute platform");
+                EnRouteManagerWrapper.Instance.Manager.registerDeviceToken("apple_background", DeviceToken);
+            }
+        
+            // Save new device token 
+            NSUserDefaults.StandardUserDefaults.SetString(DeviceToken, "PushDeviceToken");
+        }
+        
+        public override void FailedToRegisterForRemoteNotifications (UIApplication application , NSError error)
+        {
+            Debug.WriteLine("FailedToRegisterForRemoteNotifications()");
+        }
+        
+        public override void ReceivedRemoteNotification (UIApplication application, NSDictionary userInfo)
+        {
+            Debug.WriteLine("ReceivedRemoteNotification:" + userInfo.Description);
+            EnRouteManagerWrapper.Instance.Manager.handleRemoteNotification(userInfo.Description);
+        }
+        
+        public override void DidReceiveRemoteNotification (UIApplication application, NSDictionary userInfo, Action<UIBackgroundFetchResult> completionHandler)
+        {
+            Debug.WriteLine("DidReceiveRemoteNotification:" + userInfo.Description);  
+            EnRouteManagerWrapper.Instance.Manager.handleRemoteNotification(userInfo.Description);
+            completionHandler(UIBackgroundFetchResult.NewData);
         }
     }
 }
