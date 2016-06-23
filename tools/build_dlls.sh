f1162d1888657f9d6ea92c0da70c7e09306e0e6c
Nickolas Glickenhouse <nick@glympse.com>
2016-06-23 13:47:31 -0700

    Adds the client sdk version on the archive filename.

--- tools/build_dlls.sh
+++ tools/build_dlls.sh
@@ -5,6 +5,10 @@ set -e
 # This script creates a zip file containing Dlls from the Enroute Android and iOS Xamarin libraries.
 #
 
+# Figure out the client sdk version based on the zips we have
+CLIENT_SDK_ZIP=$(find . -name "EnRoute_Api_Android_*.zip" | cut -d '_' -f 4)
+CLIENT_SDK_VERSION=${CLIENT_SDK_ZIP%.zip}
+
 # Clean up previous builds
 rm -rf tmp_dlls
 mkdir tmp_dlls
@@ -25,11 +29,11 @@ cp ../source/EnRouteApiAndroid/bin/Release/EnRouteApi.dll tmp_dlls/EnRouteApi.dl
 # Build the iOS solution
 /Library/Frameworks/Mono.framework/Commands/xbuild /p:Configuration=Release /p:BuildIpa=false /target:Build ../source/EnRouteApiiOS/EnRouteApi.iOS.csproj
 
-# Move the Dlls to the output folder
+# Move iOS Dlls to the output folder
 cp ../source/EnRouteApiiOS/bin/Release/EnRouteApi.iOS.dll tmp_dlls/EnRouteApi.iOS.dll
 
 # Zip the Dlls
-TARGET_ZIP=EnRoute_Api_Xamarin.zip
+TARGET_ZIP=EnRoute_Api_Xamarin_$CLIENT_SDK_VERSION.zip
 echo "Creating archive $TARGET_ZIP" 
 pushd tmp_dlls > /dev/null
     rm -rf $TARGET_ZIP
