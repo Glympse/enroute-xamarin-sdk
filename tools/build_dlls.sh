0a47869c5ce27f21edc732b6f8fdb70bbb5d9866
Nickolas Glickenhouse <nick@glympse.com>
2016-06-23 14:53:51 -0700

    Adding version.properties which should be kept up to date with the version number for this sdk. Placed a version.properties inside the output zip showing version numbers for this sdk and the EnRoute SDK it was built against.

--- tools/build_dlls.sh
+++ tools/build_dlls.sh
@@ -5,13 +5,18 @@ set -e
 # This script creates a zip file containing Dlls from the Enroute Android and iOS Xamarin libraries.
 #
 
+# Clean up previous builds
+rm -rf tmp_dlls
+mkdir tmp_dlls
+rm -rf build
+mkdir build
+
 # Figure out the client sdk version based on the zips we have
 CLIENT_SDK_ZIP=$(find . -name "EnRoute_Api_Android_*.zip" | cut -d '_' -f 4)
 CLIENT_SDK_VERSION=${CLIENT_SDK_ZIP%.zip}
 
-# Clean up previous builds
-rm -rf tmp_dlls
-mkdir tmp_dlls
+# Get XAMARIN_SDK_VERSION
+. ../version.properties
 
 # Clean the Andriod build
 /Library/Frameworks/Mono.framework/Commands/xbuild /p:Configuration=Release /t:PackageForAndroid /target:Build ../source/EnRouteApiAndroid/EnRouteApi.Android.csproj /t:Clean
@@ -32,10 +37,16 @@ cp ../source/EnRouteApiAndroid/bin/Release/EnRouteApi.dll tmp_dlls/EnRouteApi.dl
 # Move iOS Dlls to the output folder
 cp ../source/EnRouteApiiOS/bin/Release/EnRouteApi.iOS.dll tmp_dlls/EnRouteApi.iOS.dll
 
+# Create a version.properties to package with the zip
+echo "XAMARIN_SDK_VERSION=${XAMARIN_SDK_VERSION}" > tmp_dlls/version.properties
+echo "CLIENT_SDK_VERSION=${CLIENT_SDK_VERSION}" >> tmp_dlls/version.properties
+
 # Zip the Dlls
-TARGET_ZIP=EnRoute_Api_Xamarin_$CLIENT_SDK_VERSION.zip
+TARGET_ZIP=EnRoute_Api_Xamarin_$XAMARIN_SDK_VERSION.zip
 echo "Creating archive $TARGET_ZIP" 
 pushd tmp_dlls > /dev/null
     rm -rf $TARGET_ZIP
     zip -q -r -y $TARGET_ZIP .
+    mv $TARGET_ZIP ../build/$TARGET_ZIP
 popd > /dev/null
+
