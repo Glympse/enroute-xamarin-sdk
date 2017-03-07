0927d3136ddb9b6ecb32394ef38dfb931c4dab04
Adam Hill <adam@glympse.com>
2017-03-07 13:46:56 -0800

    Unpack script was modified to work with the new Android structure and the sample app was updated with the new authentication flow.

--- samples/EnRouteDemo/Snippets/Auth.cs
+++ samples/EnRouteDemo/Snippets/Auth.cs
@@ -8,25 +8,28 @@ namespace EnRouteDemo
      */ 
     public class Auth
     {
-        private static String _driverEmail = "<< Driver Email >>";
-        private static String _driverPassword = "<< Driver Password >>";
+		private static EnRouteDemo.Listener _listener;
+
+		private static String _driverEmail = "adam@twc.com";
+        private static String _driverPassword = "ah";
 
         public static void onAppStart(GEnRouteManager manager)
         {
-            if ( manager.isLoginNeeded() ) 
-            {
-                manager.loginWithCredentials(_driverEmail, _driverPassword);
-            } 
-            else 
-            {
-                manager.start();
-            }
+			_listener = new EnRouteDemo.Listener();
+			manager.addListener(_listener);
+			manager.setAuthenticationMode(EnRouteConstants.AUTH_MODE_CREDENTIALS);
+			manager.start();
         }
 
         public static void onDriverLogout(GEnRouteManager manager)
         {
             manager.logout(EnRouteConstants.LOGOUT_REASON_USER_ACTION);
         }
+
+		public static void login(GEnRouteManager manager)
+		{
+			manager.loginWithCredentials(_driverEmail, _driverPassword);
+		}
     }
 }
 
