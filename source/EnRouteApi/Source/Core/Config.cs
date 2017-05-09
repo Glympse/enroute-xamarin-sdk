525e7b0549004d101e5a3cb7b40198ecabc39139
Adam Hill <adam@glympse.com>
2017-05-09 09:44:10 -0700

    Updating version.

--- source/EnRouteApi/Source/Core/Config.cs
+++ source/EnRouteApi/Source/Core/Config.cs
@@ -7,9 +7,9 @@ namespace EnRouteApi
         /**
          * EnRoute Xamarin SDK version.
          */
-        public static readonly int ENROUTE_SDK_MAJOR = 1;
-        public static readonly int ENROUTE_SDK_MINOR = 2;
-        public static readonly int ENROUTE_SDK_BUILD = 8;
+        public static readonly int ENROUTE_SDK_MAJOR = 2;
+        public static readonly int ENROUTE_SDK_MINOR = 0;
+        public static readonly int ENROUTE_SDK_BUILD = 9;
         public static readonly int ENROUTE_SDK_BUGFIX = 0;
         public static readonly int ENROUTE_SDK_ITER = 0;
     }
