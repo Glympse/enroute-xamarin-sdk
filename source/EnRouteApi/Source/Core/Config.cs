bcb18250752aeadda1420177d9d186eb57887e54
Adam Hill <adam@glympse.com>
2017-11-06 19:47:21 -0800

    Updating version.

--- source/EnRouteApi/Source/Core/Config.cs
+++ source/EnRouteApi/Source/Core/Config.cs
@@ -9,7 +9,7 @@ namespace EnRouteApi
          */
         public static readonly int ENROUTE_SDK_MAJOR = 2;
         public static readonly int ENROUTE_SDK_MINOR = 1;
-        public static readonly int ENROUTE_SDK_BUILD = 10;
+        public static readonly int ENROUTE_SDK_BUILD = 11;
         public static readonly int ENROUTE_SDK_BUGFIX = 0;
         public static readonly int ENROUTE_SDK_ITER = 0;
     }
