fba0b6b2e64cf65bb7d895a537828fe2f2269a8c
Nick Glickenhouse <nick@glympse.com>
2019-05-30 13:27:12 -0400

    Added EnRouteApi.iOS.Tests

---
+++ tests/EnRouteApi.iOS.Tests/UnitTestAppDelegate.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+
+using Foundation;
+using UIKit;
+using NUnit.Runner.Services;
+
+namespace EnRouteApi.iOS.Tests
+{
+    [Register("UnitTestAppDelegate")]
+    public partial class UnitTestAppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            global::Xamarin.Forms.Forms.Init();
+
+            // This will load all tests within the current project
+            var nunit = new NUnit.Runner.App();
+
+            nunit.Options = new TestOptions
+            {
+                AutoRun = true,
+                CreateXmlResultFile = true,
+                ResultFilePath = Path.Combine(NSFileManager.DefaultManager.GetUrls(NSSearchPathDirectory.LibraryDirectory, NSSearchPathDomain.User)[0].Path, "Results.xml")
+            };
+
+            LoadApplication(nunit);
+
+            return base.FinishedLaunching(app, options);
+        }
+    }
+}
