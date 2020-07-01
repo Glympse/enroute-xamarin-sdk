439001b9ff2a5bdb2cbcd15527958165533218d9
Nick Glickenhouse <nick@glympse.com>
2020-07-01 11:39:52 -0400

    AndroidX migration

--- samples/EnRouteDemo/Droid/PushReceiver.cs
+++ samples/EnRouteDemo/Droid/PushReceiver.cs
@@ -1,16 +1,9 @@
 ﻿
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 using Android.App;
 using Android.Content;
-using Android.OS;
-using Android.Runtime;
 using Android.Util;
-using Android.Views;
-using Android.Widget;
 
 using Glympse.EnRoute;
 using Glympse.EnRoute.Android;
