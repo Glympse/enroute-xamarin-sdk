373fefeb128159cc7f754da419a6b40ba395be1e
Nick Glickenhouse <nick@glympse.com>
2020-07-06 14:43:44 -0400

    Added interface for changing the XoA configuration

--- source/EnRouteApi/Source/Core/GConfig.cs
+++ source/EnRouteApi/Source/Core/GConfig.cs
@@ -5,5 +5,8 @@ namespace Glympse
     public interface GConfig : GCommon
     {
         void setActiveSharingNotificationMessage(string message);
+
+        // For possible modes see GlympseConstants::EXPIRE_ON_ARRIVAL_*
+        void setExpireOnArrival(int mode);
     }
 }
