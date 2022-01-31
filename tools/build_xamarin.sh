824bd030bde234dfddf8906767550368994547b0
Nick Glickenhouse <nick@glympse.com>
2022-01-31 15:06:27 -0500

    Testing msbuildversion option

--- tools/build_xamarin.sh
+++ tools/build_xamarin.sh
@@ -21,7 +21,7 @@ CLIENT_SDK_ZIP=$(find . -name "EnRoute_Api_Android_*.zip" | cut -d '_' -f 4)
 CLIENT_SDK_VERSION=${CLIENT_SDK_ZIP%.zip}
 
 # NuGet Restore
-/Library/Frameworks/Mono.framework/Commands/nuget restore ../samples/EnRouteDemo/EnRouteDemo.sln
+/Library/Frameworks/Mono.framework/Commands/nuget restore ../samples/EnRouteDemo/EnRouteDemo.sln -msbuildversion 15
 
 # Clean the Andriod build
 /Library/Frameworks/Mono.framework/Commands/msbuild /p:Configuration=Release /p:AndroidClassParser=class-parse /target:Clean ../source/EnRouteApiAndroid/EnRouteApi.Android.csproj
