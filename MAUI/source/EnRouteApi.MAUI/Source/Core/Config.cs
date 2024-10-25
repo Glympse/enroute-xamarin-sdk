94bbdaa20570d2dd183077bbada1f76cef5a9a7c
Nick Glickenhouse <nick@glympse.com>
2024-10-25 11:28:13 -0400

    Updated version to 2.108.276

--- MAUI/source/EnRouteApi.MAUI/Source/Core/Config.cs
+++ MAUI/source/EnRouteApi.MAUI/Source/Core/Config.cs
@@ -9,7 +9,7 @@ namespace EnRouteApi
          */
         public static readonly int ENROUTE_SDK_MAJOR = 2;
         public static readonly int ENROUTE_SDK_MINOR = 108;
-        public static readonly int ENROUTE_SDK_BUILD = 275;
+        public static readonly int ENROUTE_SDK_BUILD = 276;
         public static readonly int ENROUTE_SDK_BUGFIX = 0;
         public static readonly int ENROUTE_SDK_ITER = 0;
     }
