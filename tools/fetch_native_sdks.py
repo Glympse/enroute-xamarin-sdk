6da920fe7d102ba9e31b7d5d43d1299b9799538b
Nick Glickenhouse <nick@glympse.com>
2024-10-25 11:45:21 -0400

    Updated script for fetching native SDKs to use python3

--- tools/fetch_native_sdks.py
+++ tools/fetch_native_sdks.py
@@ -8,11 +8,11 @@
 
 import json
 import sys
-import urllib2
+import urllib.request, urllib.error, urllib.parse
 from pprint import pprint
 
 def downloadFile(url):
-	r = urllib2.urlopen(url)
+	r = urllib.request.urlopen(url)
 	CHUNK_SIZE = 1 << 20
 	with open(sdkFile, 'wb') as f:
 	    while True:
@@ -22,7 +22,7 @@ def downloadFile(url):
 	        f.write(chunk)
 
 def fetchPackageData(url):
-	response = urllib2.urlopen(url)
+	response = urllib.request.urlopen(url)
 	data = json.load(response)
 
 	build = data["resource"]["build"]
@@ -35,10 +35,10 @@ baseUrl = "https://" + bucketName + ".s3.amazonaws.com/enroute/client-sdk/downlo
 platforms = ["android", "ios"]
 
 for platform in platforms:
-	print "Downloading " + platform + " package information"
+	print("Downloading " + platform + " package information")
 	packageUrl = baseUrl + platform + "/package.json"
 	build, sdkFile = fetchPackageData(packageUrl)
 	sdkUrl = baseUrl + platform + "/" + build + "/" + sdkFile
-	print "Downloading " + sdkFile
+	print("Downloading " + sdkFile)
 	downloadFile(sdkUrl)
-print "Done"
+print("Done")
